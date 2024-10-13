27b330aa39468fd85de882e82bd6a1443ec3c7b1
Amna Noor <amnanoor7172@gmail.com>
2024-10-14 01:50:22 +0500

    initial Ev 1

---
+++ app/src/main/java/com/example/swiggy_app_bcsf21m004/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.swiggyappclone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.swiggy_app_bcsf21m004.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Set the content view to the welcome.xml layout
+        setContentView(R.layout.welcome)
+    }
+}
